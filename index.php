45fe3849cdadeed70a871872ebe1ea7aa7a2a5d0
joshsanz21 <88858069+joshsanz21@users.noreply.github.com>
2021-08-13 20:25:54 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ class PDF extends FPDF{
 //CREACION DE LA HOJA
 //$years=$_GET['years'];
 $pdf = new PDF('P', 'mm','Letter');
-$pdf->setMargins(26,18);
+$pdf->setMargins(37,18);
 $pdf->AliasNbPages();
 $pdf->AddPage();
 
@@ -20,7 +20,7 @@ $pdf->AddPage();
 $pdf->SetTextColor(0x00,0x00,0x00);
 $pdf->SetFont('Arial','b',7);
 $pdf->Cell(0,5,'JOYERIA BETY',0,1,'C');
-$pdf->Cell(0,5,'EMPLEADOS REGISTRADOS ',0,1,'C');
+$pdf->Cell(0,5,'PRODUCTOS REGISTRADOS ',0,1,'C');
 
 
   $pdf->Ln();
@@ -31,31 +31,26 @@ $pdf->Cell(0,5,'EMPLEADOS REGISTRADOS ',0,1,'C');
   
   //1 indica borde, 0 no hace salto de linea, c es centrado
 
-$result=mysqli_query($link, "select curp,fecha_nac,direccion,nombre,apellidos,email,sexo,telefono from empleados"); 
+$result=mysqli_query($link, "select nombre,stock,precioactual,categoria,proveedor from productos"); 
 
   
 //$result = mysql_query($consulta);
 $pdf->Ln();
     //aqui agregue las cabeceras de las tablas
-    $pdf->Cell(30,5, "CURP",1,0,'C');
-    $pdf->Cell(18,5, "FECHA_NAC",1,0,'C');
-    $pdf->Cell(18,5, "DIRECCION",1,0,'C');
-    $pdf->Cell(20,5, "NOMBRE",1,0,'C');
-    $pdf->Cell(28,5, "APELLIDOS",1,0,'C');
-    $pdf->Cell(32,5, "EMAIL",1,0,'C');
-    $pdf->Cell(18,5, "SEXO",1,0,'C');
-    $pdf->Cell(15,5, "TELEFONO",1,0,'C');
+    $pdf->Cell(30,5, "NOMBRE",1,0,'C');
+    $pdf->Cell(18,5, "STOCK",1,0,'C');
+    $pdf->Cell(30,5, "PRECIO ACTUAL",1,0,'C');
+    $pdf->Cell(30,5, "CATEGORIA",1,0,'C');
+    $pdf->Cell(30,5, "PROVEEDOR",1,0,'C');
 while($row = mysqli_fetch_array($result)){ 
     $pdf->Ln();
    
-    $pdf->Cell(30,5, $row['curp'],1,0,'C');
-    $pdf->Cell(18,5, $row['fecha_nac'],1,0,'C');
-    $pdf->Cell(18,5, $row['direccion'],1,0,'C');
-    $pdf->Cell(20,5, $row['nombre'],1,0,'C');
-    $pdf->Cell(28,5, $row['apellidos'],1,0,'C');
-    $pdf->Cell(32,5, $row['email'],1,0,'C');
-    $pdf->Cell(18,5, $row['sexo'],1,0,'C');
-    $pdf->Cell(15,5, $row['telefono'],1,0,'C');
+    $pdf->Cell(30,5, $row['nombre'],1,0,'C');
+    $pdf->Cell(18,5, $row['stock'],1,0,'C');
+    $pdf->Cell(30,5, $row['precioactual'],1,0,'C');
+    $pdf->Cell(30,5, $row['categoria'],1,0,'C');
+    $pdf->Cell(30,5, $row['proveedor'],1,0,'C');
+    
  
  
     //$exec=mysql_query($consulta); 
