9108a956ee8ef2597b2e4a4ffa53cb38cfe3c79e
joshsanz21 <88858069+joshsanz21@users.noreply.github.com>
2021-08-13 20:22:59 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ class PDF extends FPDF{
 //CREACION DE LA HOJA
 //$years=$_GET['years'];
 $pdf = new PDF('P', 'mm','Letter');
-$pdf->setMargins(9,18);
+$pdf->setMargins(26,18);
 $pdf->AliasNbPages();
 $pdf->AddPage();
 
@@ -20,7 +20,7 @@ $pdf->AddPage();
 $pdf->SetTextColor(0x00,0x00,0x00);
 $pdf->SetFont('Arial','b',7);
 $pdf->Cell(0,5,'JOYERIA BETY',0,1,'C');
-$pdf->Cell(0,5,'CLIENTES REGISTRADOS ',0,1,'C');
+$pdf->Cell(0,5,'EMPLEADOS REGISTRADOS ',0,1,'C');
 
 
   $pdf->Ln();
@@ -31,33 +31,31 @@ $pdf->Cell(0,5,'CLIENTES REGISTRADOS ',0,1,'C');
   
   //1 indica borde, 0 no hace salto de linea, c es centrado
 
-$result=mysqli_query($link, "select nombre,apellidopaterno,apellidomaterno,telefono,email,ciudad,colonia,calle,direccion from cliente"); 
+$result=mysqli_query($link, "select curp,fecha_nac,direccion,nombre,apellidos,email,sexo,telefono from empleados"); 
 
   
 //$result = mysql_query($consulta);
 $pdf->Ln();
     //aqui agregue las cabeceras de las tablas
-    $pdf->Cell(18,5, "NOMBRE",1,0,'C');
-    $pdf->Cell(30,5, "APELLIDO PATERNO",1,0,'C');
-    $pdf->Cell(30,5, "APELLIDO MATERNO",1,0,'C');
-    $pdf->Cell(18,5, "TELEFONO",1,0,'C');
-    $pdf->Cell(29,5, "EMAIL",1,0,'C');
-    $pdf->Cell(18,5, "CIUDAD",1,0,'C');
-    $pdf->Cell(18,5, "COLONIA",1,0,'C');
-    $pdf->Cell(18,5, "CALLE",1,0,'C');
-    $pdf->Cell(19,5, "DIRECCION",1,0,'C');
+    $pdf->Cell(30,5, "CURP",1,0,'C');
+    $pdf->Cell(18,5, "FECHA_NAC",1,0,'C');
+    $pdf->Cell(18,5, "DIRECCION",1,0,'C');
+    $pdf->Cell(20,5, "NOMBRE",1,0,'C');
+    $pdf->Cell(28,5, "APELLIDOS",1,0,'C');
+    $pdf->Cell(32,5, "EMAIL",1,0,'C');
+    $pdf->Cell(18,5, "SEXO",1,0,'C');
+    $pdf->Cell(15,5, "TELEFONO",1,0,'C');
 while($row = mysqli_fetch_array($result)){ 
     $pdf->Ln();
    
-    $pdf->Cell(18,5, $row['nombre'],1,0,'C');
-    $pdf->Cell(30,5, $row['apellidopaterno'],1,0,'C');
-    $pdf->Cell(30,5, $row['apellidomaterno'],1,0,'C');
-    $pdf->Cell(18,5, $row['telefono'],1,0,'C');
-    $pdf->Cell(29,5, $row['email'],1,0,'C');
-    $pdf->Cell(18,5, $row['ciudad'],1,0,'C');
-    $pdf->Cell(18,5, $row['colonia'],1,0,'C');
-    $pdf->Cell(18,5, $row['calle'],1,0,'C');
-    $pdf->Cell(19,5, $row['direccion'],1,0,'C');
+    $pdf->Cell(30,5, $row['curp'],1,0,'C');
+    $pdf->Cell(18,5, $row['fecha_nac'],1,0,'C');
+    $pdf->Cell(18,5, $row['direccion'],1,0,'C');
+    $pdf->Cell(20,5, $row['nombre'],1,0,'C');
+    $pdf->Cell(28,5, $row['apellidos'],1,0,'C');
+    $pdf->Cell(32,5, $row['email'],1,0,'C');
+    $pdf->Cell(18,5, $row['sexo'],1,0,'C');
+    $pdf->Cell(15,5, $row['telefono'],1,0,'C');
  
  
     //$exec=mysql_query($consulta); 
